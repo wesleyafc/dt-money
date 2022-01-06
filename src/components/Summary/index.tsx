44bbbfea847399d00e4070bc58c8b99c8c047cb7
wesleyafc <wesleyafc2010@gmail.com>
2022-01-06 13:28:58 -0300

    Summary component

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,36 @@
+import incomeImg from '../../assets/income.svg'
+import outcomeImg from '../../assets/outcome.svg'
+import totalImg from '../../assets/total.svg'
+import { Container } from "./styles";
+
+
+export function Summary() {
+    return (
+        <Container>
+            <div>
+                <header>
+                    <p>incomes</p>
+                    <img src={incomeImg} alt="incomes" />
+                </header>
+                <strong>R$ 1000,00</strong>
+            </div>
+
+            <div>
+                <header>
+                    <p>outcome</p>
+                    <img src={outcomeImg} alt="outcome" />
+                </header>
+                <strong>-R$ 500,00</strong>
+            </div>
+
+            <div className="highlight-background">
+                <header>
+                    <p>total</p>
+                    <img src={totalImg} alt="total" />
+                </header>
+                <strong>R$ 1000,00</strong>
+            </div>
+
+        </Container>
+    )
+}
