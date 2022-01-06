8b97de8005104eaf8dc5a4503f60fe8826092a1a
wesleyafc <wesleyafc2010@gmail.com>
2022-01-06 00:50:01 -0300

    styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { GlobalStyle } from './styles/global'
+
 export function App() {
   return (
+
+
     <div className="App">
       <h1>hello world</h1>
+      <GlobalStyle />
     </div>
+
   );
 }
 
